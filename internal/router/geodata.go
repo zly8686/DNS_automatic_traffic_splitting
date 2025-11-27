87ec3323f78a74b78d7e1f4f7ab0044063447f86
Sanite&Ava <y15383640831@icloud.com>
2025-11-27 19:43:28 +0800

    optimize memory calls at startup

--- internal/router/geodata.go
+++ internal/router/geodata.go
@@ -2,7 +2,9 @@ package router
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"runtime/debug"
 	"strings"
 
 	"github.com/metacubex/geo/geoip"
@@ -15,15 +17,19 @@ type GeoDataManager struct {
 }
 
 func NewGeoDataManager(geoipPath, geositePath string) (*GeoDataManager, error) {
+	log.Printf("正在加载 GeoIP 数据: %s", geoipPath)
 	geoIPData, err := geoip.FromFile(geoipPath)
 	if err != nil {
 		return nil, fmt.Errorf("无法加载 GeoIP 数据 %s: %w", geoipPath, err)
 	}
+	debug.FreeOSMemory()
 
+	log.Printf("正在加载 GeoSite 数据: %s", geositePath)
 	geoSiteData, err := geosite.FromFile(geositePath)
 	if err != nil {
 		return nil, fmt.Errorf("无法加载 GeoSite 数据 %s: %w", geositePath, err)
 	}
+	debug.FreeOSMemory()
 
 	return &GeoDataManager{
 		geoip:   geoIPData,
